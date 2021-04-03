05b13f899e4a24985eb23d8e48461b5b9f96a980
Zhi Tan <zhi@zhi.fyi>
2021-04-02 23:11:19 -0400

    Tried to make an actual tree

---
+++ TBD.Psi.TransformTree/TransformationTree.cs
@@ -0,0 +1,164 @@
+﻿
+namespace TBD.Psi.TransformTree
+{
+    using System.Linq;
+    using System.Collections.Generic;
+    using MathNet.Numerics.LinearAlgebra;
+    using MathNet.Spatial.Euclidean;
+    using Microsoft.Psi;
+
+    public class TransformationTree<T>
+    {
+
+        protected Dictionary<T, Dictionary<T, CoordinateSystem>> tree = new Dictionary<T, Dictionary<T, CoordinateSystem>>();
+
+        protected TransformationTreeNode<T> rootNode;
+
+
+        protected void traverseTree(T parent, CoordinateSystem transform, List<(T id, CoordinateSystem Pose)> frameList)
+        {
+            foreach(var child in this.tree[parent].Keys)
+            {
+                var childTransfrom = this.tree[parent][child].TransformBy(transform);
+                frameList.Add((child, childTransfrom));
+                if (this.tree.ContainsKey(child))
+                {
+                    this.traverseTree(child, childTransfrom, frameList);
+                }
+            }
+        }
+
+
+        public TransformationTree()
+        {
+        }
+
+        public List<(T, CoordinateSystem)> TraverseTree(T root, CoordinateSystem rootPose)
+        {
+            var poseList = new List<(T Id, CoordinateSystem Pose)>();
+            this.traverseTree(root, rootPose, poseList);
+            return poseList;
+        }
+
+        public bool UpdateTransformation(T frameA, T frameB, double[,] mat)
+        {
+            return this.UpdateTransformation(frameA, frameB, new CoordinateSystem(Matrix<double>.Build.DenseOfArray(mat)));
+        }
+
+        public bool UpdateTransformation(T parentKey, T childKey, CoordinateSystem transform)
+        {
+            // if there is no root make one
+            if (this.rootNode == null)
+            {
+                this.rootNode = new TransformationTreeNode<T>(parentKey, true);
+                this.rootNode.AddChild(childKey, transform);
+            }
+            else
+            {
+                // if 
+
+            }
+
+
+
+
+            if (this.tree.ContainsKey(frameA) || this.tree.ContainsKey(frameB))
+            {
+                // if this exist
+                if (this.tree.ContainsKey(frameA) && !this.tree.ContainsKey(frameB))
+                {
+                    this.tree[frameA][frameB] = transform.DeepClone();
+                    this.tree[frameB] = new Dictionary<T, CoordinateSystem>();
+                }
+                else if (!this.tree.ContainsKey(frameA) && this.tree.ContainsKey(frameB))
+                {
+                    this.tree[frameB][frameA] = transform.Invert();
+                    this.tree[frameA] = new Dictionary<T, CoordinateSystem>();
+                }
+                else
+                {
+                    // update the existing graph to prevent loop
+                    if (this.tree[frameA].ContainsKey(frameB))
+                    {
+                        this.tree[frameA][frameB] = transform.DeepClone();
+                    }
+                    else if (this.tree[frameB].ContainsKey(frameA))
+                    {
+                        this.tree[frameB][frameA] = transform.Invert();
+                    }
+                }
+            }
+            else
+            {
+                // for all of them.
+                this.tree[frameA] = new Dictionary<T, CoordinateSystem>();
+                this.tree[frameB] = new Dictionary<T, CoordinateSystem>();
+                this.tree[frameA][frameB] = transform.DeepClone();
+
+            }
+            return true;
+        }
+
+        protected CoordinateSystem recursiveSearchNode(T parent, T target)
+        {
+            // check end condition
+            if (this.tree[parent].ContainsKey(target))
+            {
+                return this.tree[parent][target];
+            }
+            
+            foreach(var child in this.tree[parent].Keys)
+            {
+                if(this.tree.ContainsKey(child))
+                {
+                    // search the children
+                    var transform = this.recursiveSearchNode(child, target);
+                    if (transform != null)
+                    {
+                        return transform.TransformBy(this.tree[parent][child]);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public bool Contains(T frame)
+        {
+            return this.tree.ContainsKey(frame);
+        }
+
+        public bool Contains(IEnumerable<T> frames)
+        {
+            return this.tree.Keys.Where(m => frames.Contains(m)).Any();
+        }
+
+        public CoordinateSystem QueryTransformation(T parentFrameKey, T childFrameKey)
+        {
+            // check if both frames are in the tree
+            if (!this.tree.ContainsKey(parentFrameKey) || !this.tree.ContainsKey(childFrameKey))
+            {
+                return null;
+            }
+
+            // if they are the same, return identity
+            if (EqualityComparer<T>.Default.Equals(parentFrameKey, childFrameKey))
+            {
+                return new CoordinateSystem();
+            }
+
+            // start from A
+            var transform = this.recursiveSearchNode(parentFrameKey, childFrameKey);
+            if(transform == null)
+            {
+                // search the otherway in case they are above it
+                transform = this.recursiveSearchNode(childFrameKey, parentFrameKey);
+                if (transform == null)
+                {
+                    return null;
+                }
+                return transform.Invert();
+            }
+            return transform;
+        }
+    }
+}
---
+++ TBD.Psi.TransformTree/TransformationTreeNode.cs
@@ -0,0 +1,61 @@
+﻿
+namespace TBD.Psi.TransformTree
+{
+    using MathNet.Spatial.Euclidean;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class TransformationTreeNode<T>
+    {
+        private bool root = false;
+        public TransformationTreeNode(T key, bool isRoot = false)
+        {
+            this.Key = key;
+            this.Transform = new CoordinateSystem();
+            this.root = isRoot;
+        }
+
+        public TransformationTreeNode(T key, CoordinateSystem transform)
+        {
+            this.Key = key;
+            this.Transform = transform;
+        }
+
+        public TransformationTreeNode(T key, TransformationTreeNode<T> parent, CoordinateSystem transform)
+        {
+            this.Key = key;
+            this.Transform = transform;
+            this.Parent = parent;
+        }
+
+        public void AddChild(T childKey, CoordinateSystem transform)
+        {
+            var newNode = new TransformationTreeNode<T>(childKey, this, transform);
+            this.Children.Add(newNode);
+        }
+
+        public bool Contains(T key)
+        {
+            if (this.Key.Equals(key))
+            {
+                return true;
+            }
+            foreach(var child in this.Children)
+            {
+                if (child.Contains(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsRoot() => this.root;
+
+        public T Key { get; set; }
+        public TransformationTreeNode<T> Parent { get; set; }
+        public List<TransformationTreeNode<T>> Children { get; private set; } = new List<TransformationTreeNode<T>>();
+        public CoordinateSystem Transform { get; set; }
+    }
+}
