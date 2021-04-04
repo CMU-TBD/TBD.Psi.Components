8bfca1f86c27a7096a350346ff3a4e24a33ef2b0
Zhi Tan <zhi@zhi.fyi>
2021-04-04 12:36:38 -0400

    Renam project

---
+++ TBD.Psi.Visualization.Windows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TBD.Psi.Visualization.Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
