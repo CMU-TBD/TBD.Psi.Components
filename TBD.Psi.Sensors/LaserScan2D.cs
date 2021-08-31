4ef8c4c0854111c1880b61a42c6b7ff124777161
Zhi Tan <zhi@zhi.fyi>
2021-08-31 13:38:43 -0400

    Added Sensor + Visualization capabilities.

---
+++ TBD.Psi.Sensors/LaserScan2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TBD.Psi.Sensors
+{
+    public class LaserScan2D
+    {
+        private readonly float[] data;
+        private readonly float maxAngle;
+        private readonly float minAngle;
+        private readonly float angleIncrement;
+        private readonly TimeSpan scanTime;
+        private readonly float maxRange;
+        private readonly float minRange;
+
+        public LaserScan2D(float[] ranges, float maxAngle, float minAngle, float angleIncrement, TimeSpan scanTime, float maxRange, float minRange)
+        {
+            this.data = ranges;
+            this.maxAngle = maxAngle;
+            this.minAngle = minAngle;
+            this.angleIncrement = angleIncrement;
+            this.scanTime = scanTime;
+            this.maxRange = maxRange;
+            this.minRange = minRange;
+        }
+
+        public float[] Ranges => this.data;
+        public float MaxAngle => this.maxAngle;
+        public float MinAngle => this.minAngle;
+        public float AngleIncrement => this.angleIncrement;
+        public TimeSpan ScanTime => this.scanTime;
+        public float MaxRange => this.maxRange;
+        public float MinRange => this.minRange;
+
+    }
+}
---
+++ TBD.Psi.Sensors/README.md
@@ -0,0 +1,8 @@
+﻿# TBD.Psi.Sensors
+COPYRIGHT(C) 2020 - Transportation, Bots, and Disability Lab - CMU
+Code released under MIT.
+
+A Psi library with classes and function related to Sensors. Some code share similarity with ROS code for interoperation purposes.
+
+# Funding
+This work is partially funded under a grant from the National Institute on Disability, Independent Living, and Rehabilitation Research (NIDILRR grant number 90DPGE0003). NIDILRR is a Center within the Administration for Community Living (ACL), Department of Health and Human Services (HHS). The contents of this website do not necessarily represent the policy of NIDILRR, ACL, HHS, and you should not assume endorsement by the Federal Government.
